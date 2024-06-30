73d264a0009c906bdc840c02c78516b281ce585e
David Huie <dahuie@gmail.com>
2024-06-30 09:34:05 -0700

    New confs

--- src/vibrato/sdkconfiguration.py
+++ src/vibrato/sdkconfiguration.py
@@ -24,9 +24,9 @@ class SDKConfiguration:
     server_idx: Optional[int] = 0
     language: str = 'python'
     openapi_doc_version: str = '0.0.1'
-    sdk_version: str = '0.1.0'
+    sdk_version: str = '0.0.2'
     gen_version: str = '2.354.2'
-    user_agent: str = 'speakeasy-sdk/python 0.1.0 2.354.2 0.0.1 vibrato-python'
+    user_agent: str = 'speakeasy-sdk/python 0.0.2 2.354.2 0.0.1 vibrato'
     retry_config: Optional[RetryConfig] = None
 
     def __post_init__(self):
